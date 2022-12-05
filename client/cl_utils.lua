36ef084fc2af40b1aeb4522046ac4a43c4f6229e
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-12-05 22:42:37 +0100

    feat(cl_utils): comma_value

--- client/cl_utils.lua
+++ client/cl_utils.lua
@@ -18,3 +18,13 @@ function deepcopy(orig, copies)
     end
     return copy
 end
+
+function comma_value(amount)
+    local numChanged
+
+    repeat
+        amount, numChanged = string.gsub(amount, '^(-?%d+)(%d%d%d)', '%1,%2')
+    until numChanged == 0
+
+    return amount
+end
