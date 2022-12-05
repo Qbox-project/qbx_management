4ba2491228ee0d666e67852a2c49ee009733d254
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-12-05 22:44:50 +0100

    cleanup: manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
-description 'qb-bossmenu'
-version '2.0.0'
+name 'qb-management'
+description 'Business and gang management menu for stashes, wardrobes and shared money'
+version '1.0.0'
+repository 'https://github.com/Qbox-project/qb-management'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
 
 client_scripts {
     'client/*.lua'
@@ -11,15 +18,6 @@ client_scripts {
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/*.lua'
-}
-
-server_exports {
-    'AddMoney',
-    'AddGangMoney',
-    'RemoveMoney',
-    'RemoveGangMoney',
-    'GetAccount',
-    'GetGangAccount',
 }
 
 lua54 'yes'
