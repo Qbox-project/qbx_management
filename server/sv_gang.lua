40e2a626daab7e5bfa87c52defb7f3e417ace6bb
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-10-04 22:56:22 -0400

    refactor(server/main): finish notify switch (#12)

--- server/sv_gang.lua
+++ server/sv_gang.lua
@@ -55,23 +55,23 @@ RegisterNetEvent('qb-gangmenu:server:FireMember', function(target)
 
 	if Employee then
 		if target ~= Player.PlayerData.citizenid then
-			if Employee.PlayerData.gang.grade.level > Player.PlayerData.gang.grade.level then TriggerClientEvent('QBCore:Notify', src, "You cannot fire this citizen!", "error") return end
+			if Employee.PlayerData.gang.grade.level > Player.PlayerData.gang.grade.level then exports.qbx_core:Notify(src, "You cannot fire this citizen!", "error") return end
 			if Employee.Functions.SetGang("none", '0') then
 				TriggerEvent("qb-log:server:CreateLog", "gangmenu", "Gang Fire", "orange", Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. ' successfully fired ' .. Employee.PlayerData.charinfo.firstname .. " " .. Employee.PlayerData.charinfo.lastname .. " (" .. Player.PlayerData.gang.name .. ")", false)
-				TriggerClientEvent('QBCore:Notify', src, "Gang Member fired!", "success")
-				TriggerClientEvent('QBCore:Notify', Employee.PlayerData.source , "You have been expelled from the gang!", "error")
+				exports.qbx_core:Notify(src, "Gang Member fired!", "success")
+				exports.qbx_core:Notify(Employee.PlayerData.source , "You have been expelled from the gang!", "error")
 			else
-				TriggerClientEvent('QBCore:Notify', src, "Error.", "error")
+				exports.qbx_core:Notify(src, "Error.", "error")
 			end
 		else
-			TriggerClientEvent('QBCore:Notify', src, "You can\'t kick yourself out of the gang!", "error")
+			exports.qbx_core:Notify(src, "You can\'t kick yourself out of the gang!", "error")
 		end
 	else
 		local player = MySQL.query.await('SELECT * FROM players WHERE citizenid = ? LIMIT 1', {target})
 		if player[1] then
 			Employee = player[1]
 			Employee.gang = json.decode(Employee.gang)
-			if Employee.gang.grade.level > Player.PlayerData.gang.grade.level then TriggerClientEvent('QBCore:Notify', src, "You cannot fire this citizen!", "error") return end
+			if Employee.gang.grade.level > Player.PlayerData.gang.grade.level then exports.qbx_core:Notify(src, "You cannot fire this citizen!", "error") return end
 			local gang = {}
 			gang.name = "none"
 			gang.label = "No Affiliation"
@@ -82,10 +82,10 @@ RegisterNetEvent('qb-gangmenu:server:FireMember', function(target)
 			gang.grade.name = nil
 			gang.grade.level = 0
 			MySQL.update('UPDATE players SET gang = ? WHERE citizenid = ?', {json.encode(gang), target})
-			TriggerClientEvent('QBCore:Notify', src, "Gang member fired!", "success")
+			exports.qbx_core:Notify(src, "Gang member fired!", "success")
 			TriggerEvent("qb-log:server:CreateLog", "gangmenu", "Gang Fire", "orange", Player.PlayerData.charinfo.firstname .. " " .. Player.PlayerData.charinfo.lastname .. ' successfully fired ' .. Employee.PlayerData.charinfo.firstname .. " " .. Employee.PlayerData.charinfo.lastname .. " (" .. Player.PlayerData.gang.name .. ")", false)
 		else
-			TriggerClientEvent('QBCore:Notify', src, "Civilian is not in city.", "error")
+			exports.qbx_core:Notify(src, "Civilian is not in city.", "error")
 		end
 	end
 	TriggerClientEvent('qb-gangmenu:client:OpenMenu', src)
@@ -100,8 +100,8 @@ RegisterNetEvent('qb-gangmenu:server:HireMember', function(recruit)
 	if not Player.PlayerData.gang.isboss then ExploitBan(src, 'HireEmployee Exploiting') return end
 
 	if Target and Target.Functions.SetGang(Player.PlayerData.gang.name, 0) then
-		TriggerClientEvent('QBCore:Notify', src, "You hired " .. (Target.PlayerData.charinfo.firstname .. ' ' .. Target.PlayerData.charinfo.lastname) .. " come " .. Player.PlayerData.gang.label .. "", "success")
-		TriggerClientEvent('QBCore:Notify', Target.PlayerData.source , "You have been hired as " .. Player.PlayerData.gang.label .. "", "success")
+		exports.qbx_core:Notify(src, "You hired " .. (Target.PlayerData.charinfo.firstname .. ' ' .. Target.PlayerData.charinfo.lastname) .. " come " .. Player.PlayerData.gang.label .. "", "success")
+		exports.qbx_core:Notify(Target.PlayerData.source , "You have been hired as " .. Player.PlayerData.gang.label .. "", "success")
 		TriggerEvent('qb-log:server:CreateLog', 'gangmenu', 'Recruit', 'yellow', (Player.PlayerData.charinfo.firstname .. ' ' .. Player.PlayerData.charinfo.lastname).. ' successfully recruited ' .. Target.PlayerData.charinfo.firstname .. ' ' .. Target.PlayerData.charinfo.lastname .. ' (' .. Player.PlayerData.gang.name .. ')', false)
 	end
 	TriggerClientEvent('qb-gangmenu:client:OpenMenu', src)
