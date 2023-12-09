d068bcf01d460b23bf0b3318941b9f1c5d68e252
Matthew <22198949+MafewTM@users.noreply.github.com>
2023-12-08 23:12:13 -0500

    fix: remove all jobs

--- config/shared.lua
+++ config/shared.lua
@@ -1,41 +1,5 @@
 return {
     menus = {
-        police = {
-            coords = vec3(447.04, -974.01, 30.44),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 183.03,
-            type = 'job'
-        },
-        ambulance = {
-            coords = vec3(337.06, -592.88, 43.29),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 155.05,
-            type = 'job'
-        },
-        realestate = {
-            coords = vec3(-716.11, 261.21, 84.14),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 0.00,
-            type = 'job'
-        },
-        taxi = {
-            coords = vec3(894.94, -179.27, 74.70),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 56.89,
-            type = 'job'
-        },
-        cardealer = {
-            coords = vec3(-30.12, -1106.30, 26.23),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 321.99,
-            type = 'job'
-        },
-        mechanic = {
-            coords = vec3(-347.49, -133.32, 39.01),
-            size = vec3(1.5, 1.5, 1.5),
-            rotation = 73.32,
-            type = 'job'
-        },
         lostmc = {
             coords = vec3(983.69, -90.92, 74.85),
             size = vec3(1.5, 1.5, 1.5),
