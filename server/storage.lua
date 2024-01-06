7598e5c3594dfa3754fcf1783269b4ad6103c32d
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-01-06 02:26:09 -0500

    fix: newly hired players not immediately showing up on boss menu

--- server/storage.lua
+++ server/storage.lua
@@ -1,23 +1,34 @@
+---@alias GroupType 'gang' | 'job'
+
 ---Fetches DB Player Entities by Group
 ---@param name string
----@param type string
----@return Players[]
+---@param type GroupType
+---@return table[]
 function FetchPlayerEntitiesByGroup(name, type)
-    return MySQL.query.await("SELECT * FROM `players` WHERE ?? LIKE '%"..name.."%'", {type})
+    local chars = {}
+    local result = MySQL.query.await("SELECT citizenid, charinfo, gang, job FROM `players` WHERE JSON_VALUE(?,'$.name') = ?", {type, name})
+    for i = 1, #result do
+        chars[i] = result[i]
+        chars[i].charinfo = json.decode(result[i].charinfo)
+        chars[i].job = result[i].job and json.decode(result[i].job)
+        chars[i].gang = result[i].gang and json.decode(result[i].gang)
+    end
+
+    return chars
 end
 
 ---Fetches DB Player Entity by CitizenId
 ---@param citizenId string
----@return Player[]
+---@return table[]
 function FetchPlayerEntityByCitizenId(citizenId)
     return MySQL.query.await('SELECT * FROM players WHERE citizenid = ? LIMIT 1', {citizenId})
 end
 
 ---Updates DB Player Entity
 ---@param citizenId string
----@param type gang | job
+---@param type GroupType
 ---@param role Gang | Job
----@return Player[]
+---@return table[]
 function UpdatePlayerJob(citizenId, type, role)
     return MySQL.update.await('UPDATE players SET '..type..' = ? WHERE citizenid = ?', {json.encode(role), citizenId})
 end
