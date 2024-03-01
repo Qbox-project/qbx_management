9cf4741010f398cc5668c0fe2fe710428a14a4e6
Manason <clanerp@gmail.com>
2024-03-01 10:03:56 -0800

    feat: support managing non primary jobs and gangs
    * feat: support managing not primary jobs and gangs
    
    * fixes after testing
    
    * fix indentation
    
    * cleanup the query

--- server/storage.lua
+++ server/storage.lua
@@ -1,18 +1,8 @@
 ---@alias GroupType 'gang' | 'job'
 
----Fetches DB Player Entities by Group
 ---@param name string
 ---@param type GroupType
----@return table[]
-function FetchPlayerEntitiesByGroup(name, type)
-    local chars = {}
-    local result = MySQL.query.await("SELECT citizenid, charinfo, gang, job FROM `players` WHERE JSON_VALUE("..type..", '$.name') = ?", {name})
-    for i = 1, #result do
-        chars[i] = result[i]
-        chars[i].charinfo = json.decode(result[i].charinfo)
-        chars[i].job = result[i].job and json.decode(result[i].job)
-        chars[i].gang = result[i].gang and json.decode(result[i].gang)
-    end
-
-    return chars
+---@return {citizenid: string, grade: integer}[]
+function FetchPlayersInGroup(name, type)
+    return MySQL.query.await("SELECT citizenid, grade FROM player_groups WHERE `group` = ? AND `type` = ?", {name, type})
 end
