753704a18d8e7092ed291a22b7c4fd9edceb7254
root <60280452+0Programmer@users.noreply.github.com>
2024-09-15 14:05:57 +0200

    chore: modify column definitions and optimize indexes

--- qbx_management.sql
+++ qbx_management.sql
@@ -1,12 +1,11 @@
-CREATE TABLE IF NOT EXISTS `player_jobs_activity`  (
+CREATE TABLE IF NOT EXISTS `player_jobs_activity` (
   `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
-  `citizenid` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
-  `job` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `citizenid` varchar(50) NOT NULL,
+  `job` varchar(255) NOT NULL,
   `last_checkin` int NOT NULL,
   `last_checkout` int NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `id`(`id` DESC) USING BTREE,
-  INDEX `citizenid`(`citizenid` ASC) USING BTREE,
-  INDEX `job`(`job` ASC) USING BTREE,
-  INDEX `last_checkout`(`last_checkout` ASC) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 0 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+  INDEX `id` (`id` DESC) USING BTREE,
+  INDEX `last_checkout` (`last_checkout` ASC) USING BTREE,
+  INDEX `citizenid_job` (`citizenid`, `job`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci;
